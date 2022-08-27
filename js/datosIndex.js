bf24f8209819b00e64cde9e25c690a165c27b0f4
Your Name~ <wgcampos15@gmail.com>
2022-08-27 17:41:31 -0500

    cambio de api

--- js/datosIndex.js
+++ js/datosIndex.js
@@ -2,31 +2,30 @@ const obtenerDatos = json =>{
     
     json.then(monedas =>{
         const arregloDeCriptomonedas = new Array();
-      //  console.table(monedas);
+        
         monedas.map(moneda => {
-            console.log(`${moneda.name}`)
-            
-            /*
-            if (moneda.rank<=5) {
+            //console.log(`${moneda.market_data.current_price.mxn}`)
+           
+            if (moneda.market_data.market_cap_rank<=10) {
               
-                const {id,name,rank,price,logo_url} = moneda;
+                const {id,symbol,market_data,image} = moneda;
                 
                 //limitamos cantidad de decimales
                 
-                
+         //     console.log(moneda);
                
-                
+                //.current_price.mxn
         
-              const cripto = new Criptomoneda(id,name,rank,price,logo_url);
-            
-            arregloDeCriptomonedas.push(cripto);
+              const cripto = new Criptomoneda(symbol,id,market_data.market_cap_rank,market_data.current_price.mxn,image.large);
+            console.log(cripto);
+          arregloDeCriptomonedas.push(cripto);
             
                  
             }
-            */
+          
         });
         
-        agregarDatosDom(arregloDeCriptomonedas);
+      agregarDatosDom(arregloDeCriptomonedas);
     })
 
 }
